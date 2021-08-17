74b8525f1f577f317a871e097726d902a1f7e536
Lionel <fullstackbeaver@gmail.com>
2021-08-17 11:55:52 +0200

    séance du 17 aout

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,15 @@ const browserSync = require('browser-sync').create();
 const gulp = require('gulp');
 const gulpConcat = require("gulp-concat");
 const sass = require('gulp-sass')(require('sass'));
-var ejs = require("gulp-ejs")
+const ejs = require("gulp-ejs");
+const sourcemaps = require('gulp-sourcemaps');
 
 function makeCss() {
   return gulp.src(['./src/base.scss', './src/**/*.scss'])
     .pipe(gulpConcat("style.css"))
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
     .pipe(autoprefixer({ cascade: false }))
     .pipe(gulp.dest('./www/css'));
 };
